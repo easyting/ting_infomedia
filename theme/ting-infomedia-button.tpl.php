87f5024a0bc12bbbdfc9fb1c4c8df80b39b42a62
JrgenGNielsen <jgn@dbc.dk>
2013-04-16 16:12:59 +0200

    add link to 'not logged in' msg

--- theme/ting-infomedia-button.tpl.php
+++ theme/ting-infomedia-button.tpl.php
@@ -1,7 +1,10 @@
 <a href="<?php print $ting_infomedia_button['#href']; ?>"
-   class="text-small text-darkgrey">
-    <span class="icon icon-left icon-darkgrey-addbasket">&nbsp;</span><span
-        class="link-add-basket cart-title"
-        data-pid="<?php print $ting_infomedia_button['#data-pid']; ?>"><?php print $ting_infomedia_button['#title']; ?>
-    </span>
+   class="infomedia-button text-small text-darkgrey">
+  <span class="icon icon-left icon-darkgrey-infomedia">&nbsp;</span>
+  <span class="link-infomedia infomedia-title" data-pid="<?php print $ting_infomedia_button['data-pid']; ?>"><?php print $ting_infomedia_button['#title']; ?></span>
 </a>
+
+<div class='element-wrapper visuallyhidden infomedia-wrapper'>
+  <span class="user-msg"><?php print $ting_infomedia_button['user-message']; ?></span>
+  <span class='close icon icon-left icon-red-x'>&nbsp;</span>
+</div>
