6e4f592cababe9f9d18deef02f18b09175373cce
JrgenGNielsen <jgn@dbc.dk>
2013-04-16 13:31:42 +0200

    updating infomedia module for ting_openformat

---
+++ theme/ting-infomedia-button.tpl.php
@@ -0,0 +1,7 @@
+<a href="<?php print $ting_infomedia_button['#href']; ?>"
+   class="text-small text-darkgrey">
+    <span class="icon icon-left icon-darkgrey-addbasket">&nbsp;</span><span
+        class="link-add-basket cart-title"
+        data-pid="<?php print $ting_infomedia_button['#data-pid']; ?>"><?php print $ting_infomedia_button['#title']; ?>
+    </span>
+</a>
