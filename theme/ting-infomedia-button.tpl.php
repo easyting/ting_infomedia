955d50bf6375354d3c7f66bb02fc5492bc827889
JrgenGNielsen <jgn@dbc.dk>
2013-04-18 14:29:07 +0200

    make flow for checking user's library data

--- theme/ting-infomedia-button.tpl.php
+++ theme/ting-infomedia-button.tpl.php
@@ -1,10 +1,11 @@
-<a href="<?php print $ting_infomedia_button['#href']; ?>"
-   class="infomedia-button text-small text-darkgrey">
+<a href="<?php print $ting_infomedia_button['#href'] ?>"
+   class="<?php print $ting_infomedia_button['#popup'] ?>infomedia-button text-small text-darkgrey"
+   rel="infomedia">
   <span class="icon icon-left icon-darkgrey-infomedia">&nbsp;</span>
-  <span class="link-infomedia infomedia-title" data-pid="<?php print $ting_infomedia_button['data-pid']; ?>"><?php print $ting_infomedia_button['#title']; ?></span>
+  <span class="link-infomedia infomedia-title" data-pid="<?php print $ting_infomedia_button['#data-pid'] ?>"><?php print $ting_infomedia_button['#title'] ?></span>
 </a>
 
 <div class='element-wrapper visuallyhidden infomedia-wrapper'>
-  <span class="user-msg"><?php print $ting_infomedia_button['user-message']; ?></span>
+  <span class="user-msg"><?php print $ting_infomedia_button['#user-message'] ?></span>
   <span class='close icon icon-left icon-red-x'>&nbsp;</span>
 </div>
