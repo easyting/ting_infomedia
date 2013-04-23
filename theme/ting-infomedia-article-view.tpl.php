44337a425bf75cddb43fdc5f8634bf8bcb5bfc8d
JrgenGNielsen <jgn@dbc.dk>
2013-04-23 12:34:01 +0200

    styling, copyright notice and link to library list if not logged in

--- theme/ting-infomedia-article-view.tpl.php
+++ theme/ting-infomedia-article-view.tpl.php
@@ -3,4 +3,7 @@
     <?php print theme('image', array('path' => drupal_get_path('module', 'ting_infomedia') . '/img/ting_infomedia_logo.jpg')); ?>
   </div>
   <?php echo $variables['element']['#markup']; ?>
+  <div class="infomedia-footer">
+    <p><?php print t('infomedia_copyright_notice', array(), array('context' => 'ting_infomedia')); ?></p>
+  </div>
 </div>
